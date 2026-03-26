58662aa538ff16243bcda6560e1280dc56ef4acc
dfox97 <danny.fox97@hotmail.com>
2026-03-26 21:25:31 +0000

    nice image update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  adapter: netlify({
+    imageCDN: true,
+  }),
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+    domains: [],
+    remotePatterns: [],
+  },
+});
